58d2777100437452214b5c9c56b4c640a848b709
RAFAELARTUROGUTIERRE <rafael.gutierrez4666@alumnos.udg.mx>
2025-02-18 00:04:42 -0600

    Backend.py done. conection with the main.py ui done.
    Need more optimization

--- Backend.py
+++ Backend.py
@@ -1,3 +1,42 @@
 import glob
+from nltk.sentiment import SentimentIntensityAnalyzer
+import nltk
 
-def get_data():
+def get_date_data():
+    names = []
+    for name in glob.glob('diary/*.txt'):
+        names.append(name)
+    return names
+
+def getPosNeg(names):
+    analizer = SentimentIntensityAnalyzer()
+    pos = []
+    neg = []
+    for name in names:
+        with open(name, 'r') as file:
+            entry = file.read()
+            score = analizer.polarity_scores(entry)
+            print(score)
+            pos.append(score['pos'])
+            neg.append(score['neg'])
+
+    return pos, neg
+
+
+def dates_again():
+    dates = get_date_data()
+    dates_ok = []
+    for date in dates:
+        dates_ok.append(date[6:-4])
+    return dates_ok
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import streamlit as st
 import plotly.express as px
+import Backend
 import nltk
 
+from Backend import dates_again
+
+dates = Backend.get_date_data()
+
+
+
+pos, neg = Backend.getPosNeg(dates)
+
+parse_dates = dates_again()
 
 st.title("Mood Across Days")
 st.header('Positivity')
-figure_po = px.line()
+figure_po = px.line(x=parse_dates, y=pos, labels={'x': 'Date', 'y': 'Positivity'})
 st.plotly_chart(figure_po)
 
 st.header('Negativity')
-figure_ne = px.line()
+figure_ne = px.line(x=parse_dates, y=neg, labels={'x': 'Date', 'y': 'Negativity'})
 st.plotly_chart(figure_ne)
 
